d75d3a46c01d000b20374f2eef7625bab1fa39cc
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-07-01 01:32:41 +0600

    implement full file and folder management CLI with fs modul

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const command = process.argv[2];
 const folderName = process.argv[3];
 const filename = process.argv[4];
 const fileValue = process.argv[5];
-// log("command:", command, "folderName:", folderName, "filename:", filename, "fileValue:", fileValue);
 
 switch (command) {
   case "write": {
@@ -45,19 +44,115 @@ switch (command) {
     break;
   }
   case "read": {
-    console.log("Read done");
+    const targetPath = path.join(__dirname, folderName, filename);
+    const isFileAvailable = fs.existsSync(targetPath);
+    if (isFileAvailable) {
+      fs.readFile(targetPath, "utf-8", (error, data) => {
+        if (error) {
+          console.log("fail to read file", error);
+        } else {
+          console.log("file read successfully");
+          console.log(data);
+        }
+      });
+    } else {
+      console.log("file not found");
+    }
     break;
   }
   case "list": {
-    console.log("list done");
+    const targetPath = path.join(__dirname, folderName);
+    const isFolderAvailable = fs.existsSync(targetPath);
+    if (isFolderAvailable) {
+      fs.readdir(targetPath, (error, files) => {
+        if (error) {
+          console.log("fail to read folder", error);
+        } else {
+          console.log("folder read successfully");
+          files.forEach((file) => {
+            console.log(file);
+          });
+        }
+      });
+    } else {
+      console.log("folder not found");
+    }
     break;
   }
   case "remove": {
-    console.log("remove done");
+    const targetFilePath = path.join(__dirname, folderName, filename);
+    const isFileAvailable = fs.existsSync(targetFilePath);
+
+    if (isFileAvailable) {
+      fs.unlink(targetFilePath, (error) => {
+        if (error) {
+          console.log("fail to delete file", error);
+        } else {
+          console.log("file deleted successfully");
+        }
+      });
+    } else {
+      console.log("file not found");
+    }
+    break;
+  }
+  case "update": {
+    const targetFilePath = path.join(__dirname, folderName, filename);
+    const isFileAvailable = fs.existsSync(targetFilePath);
+    if (isFileAvailable) {
+      fs.writeFile(targetFilePath, fileValue, (error) => {
+        if (error) {
+          console.log("fail to update file", error);
+        } else {
+          console.log("file updated successfully");
+        }
+      });
+    } else {
+      console.log("file not found");
+    }
+    break;
+  }
+  case "addNewValue": {
+    const targetFilePath = path.join(__dirname, folderName, filename);
+    const isFileAvailable = fs.existsSync(targetFilePath);
+    if (isFileAvailable) {
+      fs.appendFile(
+        targetFilePath,
+        `\nAdd At : ${new Date().toLocaleDateString()} --> ${fileValue}`,
+        (error) => {
+          if (error) {
+            console.log("fail to add new value to file", error);
+          } else {
+            console.log("new value added successfully");
+          }
+        }
+      );
+    } else {
+      console.log("file not found");
+    }
+    break;
+  }
+  case "rename": {
+    const oldFilePath = path.join(__dirname, folderName, filename);
+    const newFilePath = path.join(__dirname, folderName, fileValue);
+    const isFileAvailable = fs.existsSync(oldFilePath);
+
+    if (isFileAvailable) {
+      fs.rename(oldFilePath, newFilePath, (error) => {
+        if (error) {
+          console.log("fail to rename file", error);
+        } else {
+          console.log("file renamed successfully");
+        }
+      });
+    } else {
+      console.log("file not found");
+    }
     break;
   }
   default: {
-    console.log("something is wrong");
+    console.log("❗ Unknown command");
+    console.log("Try: write, read, list, remove, update, addNewValue, rename");
     break;
   }
 }
